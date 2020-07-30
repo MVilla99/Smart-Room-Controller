e10a87e71d5e024b0ada683e91b7e7c870b35b25
mvilla36@cnm.edu <mvilla36@cnm.edu>
2020-07-30 15:54:37 -0600

    sixth commit

--- Smart-room-controller/Smart-room-controller.ino
+++ Smart-room-controller/Smart-room-controller.ino
@@ -3,24 +3,48 @@
 #include <ACROBOTIC_SSD1306.h>
 #include <OneButton.h>
 #include <Encoder.h>
-#include <menuSelect.h>
+  Encoder enc(15,16);
+#include <interfaceMenu.h>
+#include <interfaceSelect.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <mac.h>
+#include <Adafruit_BME280.h>
+#include <Adafruit_Sensor.h>
   #define imageWidth 128
   #define imageHeight 64
-//Encoder enc(15,16);
-OneButton button1(17,false);
+  #define SEALEVELPRESSURE_HPA (1013.25)
+    Adafruit_BME280 bme;   
+    float temp;
+    float pres;
+    float hum;
+    float alt;
+int encButton = 17;
+OneButton button1(encButton,false); // try with normal button 
 bool buttonState = HIGH;
+bool DBclick = LOW;
 int i;
+
 void setup() {
   // put your setup code here, to run once:
+//pinMode(encButton, INPUT);
 Serial.begin(9600);
+Ethernet.begin(mac);
 while(!Serial);
 Wire.begin();
 oled.init();
 oled.clearDisplay();
+  unsigned status;
+  status = bme.begin(0x76);
+  
 button1.attachClick(click1);
+button1.attachDoubleClick(doubleclick1);
 button1.setClickTicks(250);
 button1.setPressTicks(2000);
-Menu(); // when bitmap clears the screen it clears Menu
+  temp = (bme.readTemperature() * 9/5)+32;
+  pres = (bme.readPressure() /100.0f)/68.9476;
+  alt = bme.readAltitude(SEALEVELPRESSURE_HPA)*3.28084;
+  hum = bme.readHumidity();
 }
 
 void loop() {
@@ -31,28 +55,51 @@ button1.tick();
   switch(i){
     case 0:
     Menu();
-    Select();
+    Select1();
       break;
     case 1:  // use enc "ticks" mapped to each S-C statement and the button will act as the select
-      Serial.println("zero");
+      Serial.println("one");
+      Menu2();
+      Select2();
       break;
     case 2:
-      Serial.println("one");
+      Serial.println("two");
       Menu3();
-      delay(1000);
+      Select3();
+      //delay(1000);
       break;
     case 3:
-      Serial.println("two");
+      Serial.println("three");
+      //ethernetStat();
+      bmeVal(temp);
+      Select4();
       break;
     case 4:
-      Serial.println("three");
+      Serial.println("four");
       break;  
   }
 
 }
 void click1(){
   buttonState =! buttonState;
-  i = (enc.read()/24);
-
- 
+  i = (enc.read()/24); 
+}
+void doubleclick1(){
+  DBclick =! DBclick;
+  oled.clearDisplay();
+  //enc.write(0);
+  //oled.clearDisplay();
+  i=0;
+}
+void bmeVal(float ptemp){
+  //oled.clearDisplay();
+  oled.setTextXY(0,0);
+  oled.putString(float(ptemp));
+}
+void ethernetStat(){
+Serial.print("my IP address: ");
+  for(byte myByte = 0; myByte < 4; myByte++){
+    Serial.print(Ethernet.localIP()[myByte],DEC);
+    Serial.print(".");    
+  }  
 }
