9195253dd5bb8ba2d100155c3c86a320cc587184
mvilla36@cnm.edu <mvilla36@cnm.edu>
2020-07-30 18:49:42 -0600

    integrating all the code. were almost there!

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Smart-Room-Controller
 smart room controller for use with wemo smart plugs and hue lightbulbs. ( among other things).
+
+At time of writing (7-30-2020), the most recent version of the code is smart room controller V2.
+writing this interface has taken my soul.
--- Smart-room-controllerV2/SRCV2/SRCV2.ino
+++ Smart-room-controllerV2/SRCV2/SRCV2.ino
@@ -35,18 +35,23 @@ bool subMenu = false;
 int i;
 int s;
 static int n;
+int pwr;
   int bulb;
   bool activated;
   bool onoff = false;
   bool last = HIGH;
+int ledPin = 8;
 Adafruit_NeoPixel pixel(12, 20, NEO_GRB + NEO_KHZ800);
 int showTemp;
 int colorShift[] = {blue4,blue3,blue2,blue1,blue0,mid1,mid2,red0,red1,red2,red3,red4}; 
   bool smartControl = false;
   bool smartMenu = false;
+  bool pwrMenu = false;
+  bool hueState = false;
 void setup() {
   // put your setup code here, to run once:
 pinMode(menuButton, INPUT);
+pinMode(ledPin, OUTPUT);
 pinMode(10, OUTPUT);
 pixel.begin();
 pixel.show();
@@ -70,7 +75,7 @@ void loop() {
   pres = (bme.readPressure() /100.0f)/68.9476;
   alt = bme.readAltitude(SEALEVELPRESSURE_HPA)*3.28084;
   hum = bme.readHumidity();
-//  hueSet();
+
 button1.tick();
   subMenu = false;
   smartMenu = false;
@@ -118,26 +123,60 @@ button1.tick();
           case 1:
           SSMyes();
          smartControl = true;
+         hueState = true;
+         delay(5000);
+         s=0;
+         i=0;
+         oled.clearDisplay();
          break;
           case 2:
-          //no function, s=0 and smartControl = false;
+          //no function, s=0 and smartControl = false
+           SSMno();
+          smartControl = false;
+          delay(5000);
+          s=0;
+          i=0;
+          oled.clearDisplay();
+          
          break;
-        }
-   
+        }  
       break;
     case 3:
       userManual(); // doesnt return to main. maybe because no scroll funct.?
       // put smart control func. in here. 
-      Select4();
+      delay(10000);
+      oled.clearDisplay();
+      i=0;
       Serial.println("three");
       break;
     case 4:
-      powerSel();
+      pwrMenu = true;
+        switch(pwr){
+          
+          case 0:
+          SelectPWR();
+          powerSel();
+            break;
+          case 1:
+          oled.setTextXY(0,0);
+          oled.putString("powering off");
+          oled.setTextXY(2,0);
+          oled.putString("thank you for");
+          oled.setTextXY(4,0);
+          oled.putString("use of this SRC");
+            break;
+          case 2:
+            break;
+        }
       Serial.println("four");
       break;  
   }
   Serial.print(n);
+ smartStateLed();
  // wemoSet();
+ //hueSet();
+ NeoRing();
+
 }
 void click1(){
   buttonState =! buttonState;
@@ -149,10 +188,14 @@ void click1(){
             if(smartMenu == true){
             s = (enc.read()/48);
             }
-   else{
+    }
+    else if (pwrMenu == true){
+      pwr = (enc.read()/48);
+    }
+   else {
       i = (enc.read()/24);
     }
-    }
+    
 
 }
 void doubleclick1(){
@@ -163,6 +206,15 @@ void doubleclick1(){
   n=0;
   i=0;
 }
+void smartStateLed(){
+ if(smartControl == true){
+  digitalWrite(ledPin, HIGH);
+}
+ if(smartControl == false){
+  digitalWrite(ledPin,LOW);
+ }
+}
+
 void bmeValT(float ptemp){
   oled.setTextXY(0,0);
   oled.putString(float(ptemp));
@@ -211,7 +263,7 @@ void wemoSet(){
 }
 /*
 void hueSet(){
-  if( == true){
+  if( hueState == true){ // just turn on for when Neopix turn on 
   onoff =! onoff;
   }
   if(onoff==true){
@@ -221,27 +273,20 @@ void hueSet(){
   setHue(bulb, activated,colorshift[showTemp],luminosity()); //photoresistor value
   }
   if(smartControl == false){
-  setHue(bulb, activated,colorshift[showTemp],100); //photoresistor value
+  setHue(bulb, activated,colorshift[showTemp],250); //photoresistor value
   }
   //^^ get color from neopixel color array
  // ^^ and brightness from photoresistor 
-  */
-//}
-int luminosity(){
-  int pResistor = 14; //or whatever number signal is
-  int pSenseVal;
-  int bitSenseVal;
-  pSenseVal = digitalRead(pResistor);
-  bitSenseVal = map(pSenseVal, 0, 3000, 0, 255);  
-  return bitSenseVal;
+  
 }
-void pixRing(){
-  int showTemp;
+*/
+void NeoRing(){
+  //int showTemp;
     showTemp = temp;
     showTemp = map(showTemp, 0, 100, 0, 12);
     if (smartControl == false){ // smart control is name of intelligent control func.
     pixel.setPixelColor(showTemp,colorShift[showTemp]);
-    pixel.setBrightness(100);//use photo resistor value
+    pixel.setBrightness(250);//use photo resistor value
     pixel.show();
     }
     if (smartControl == true){
@@ -249,4 +294,13 @@ void pixRing(){
     pixel.setBrightness(luminosity());//use photo resistor value
     pixel.show();
     }
+}
+
+unsigned int luminosity(){
+  int pResistor = 14; //or whatever number signal is
+  int pSenseVal;
+  int bitSenseVal;
+  pSenseVal = digitalRead(pResistor);
+  bitSenseVal = map(pSenseVal, 0, 10230, 0, 255);  // average looks like 5k so maybe map that
+  return bitSenseVal;
 }
